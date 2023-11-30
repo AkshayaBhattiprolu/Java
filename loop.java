d58349368657076095db9c98a6bf1454b5516b01
AkshayaBhattiprolu <150900320+AkshayaBhattiprolu@users.noreply.github.com>
2023-11-30 17:28:39 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,14 @@
+package do_whileloop;
+
+public class loop {
+
+	public static void main(String[] args) {
+int i= 6; 
+do {
+	System.out.println("numbs are:"+i);
+	i++;
+}
+while(i<=4);
+	}
+
+}
---
+++ operators.java
@@ -0,0 +1,24 @@
+package operators;
+
+public class operators {
+
+	public static void main(String[] args) {
+		int S = 8;
+		int B = 67;
+		System.out.println(S*B);
+		System.out.println(S<B);
+		System.out.println(S++);
+		System.out.println(S);
+		System.out.println(S--);
+		System.out.println(S);
+		System.out.println(S<<2);
+		System.out.println(S>>2);
+
+		
+	
+		
+		
+
+	}
+
+}
